684c81be9564338039a5005af58cca9294f45f5e
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-21 15:44:44 -0300

    feat: handle multiples errors on Create.ts

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,26 +1,33 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup';
 
 interface ICity {
-  name: string
+  nome: string,
+  estado: string
 }
 
 const bodyValidation: yup.Schema<ICity> = yup.object().shape({
-  name: yup.string().required().min(3)
+  nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3)
 })
 
 export const create  = async (req: Request<{}, {}, ICity>, res: Response) => {
   let validateData: ICity | undefined = undefined
 
   try {
-    validateData = await bodyValidation.validate(req.body)
-  } catch (error) {
-    const yupError = error as yup.ValidationError
-    return res.json({errors: {default: yupError.message}})
-  }
+    validateData = await bodyValidation.validate(req.body, {abortEarly: false})
+  } catch (err) {
+    const yupError = err as yup.ValidationError
+    const errors: Record<string, string> = {}
 
-  console.log(validateData);
-  
-  
-  return res.send('create')
+    yupError.inner.forEach((error) => {
+      if (!error.path) return
+      errors[error.path] = error.message
+    })
+
+    return res.status(StatusCodes.BAD_REQUEST).json({errors: errors})
+  }  
+
+  return res.send('Criado com sucesso!')
 }
