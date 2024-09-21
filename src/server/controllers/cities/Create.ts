6d4b6ff28c89017d6e5cb9b471e4a724366f6973
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-21 15:24:29 -0300

    feat: handle name validation using yup

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from "express";
+import * as yup from 'yup';
 
 interface ICity {
   name: string
 }
 
-export const create  = (req: Request<{}, {}, ICity>, res: Response) => {
-  console.log(req.body.name);
+const bodyValidation: yup.Schema<ICity> = yup.object().shape({
+  name: yup.string().required().min(3)
+})
+
+export const create  = async (req: Request<{}, {}, ICity>, res: Response) => {
+  let validateData: ICity | undefined = undefined
+
+  try {
+    validateData = await bodyValidation.validate(req.body)
+  } catch (error) {
+    const yupError = error as yup.ValidationError
+    return res.json({errors: {default: yupError.message}})
+  }
+
+  console.log(validateData);
+  
   
   return res.send('create')
 }
