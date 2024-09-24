cc3d57fd175e71ec8238b649e2aea9d5efd2012a
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-24 09:47:20 -0300

    feat: delete city controller

--- src/server/controllers/cities/DeleteById.ts
+++ src/server/controllers/cities/DeleteById.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
+import { CitiesProvider } from "../../database/providers/cities";
 import { validation } from "../../shared/middleware";
 
 interface IParamsProps {
@@ -17,12 +18,22 @@ export const deleteByIdBodyValidation = validation((getSchema) => ({
 }));
 
 export const deleteById = async (req: Request<IParamsProps>, res: Response) => {
-  if (Number(req.params.id) === 99999)
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+  if (!req.params.id)
+    return res.status(StatusCodes.BAD_REQUEST).json({
       errors: {
-        default: "Registro não encontrado.",
+        default: "O parâmetro ID precisa ser informado.",
       },
     });
+
+  const result = await CitiesProvider.deleteById(req.params.id);
+
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message,
+      },
+    });
+  }
 
   return res.status(StatusCodes.NO_CONTENT).send();
 };
