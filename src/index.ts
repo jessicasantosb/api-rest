d820c9620235234c94f9a5ed3e8d75281127c0ab
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-24 11:01:28 -0300

    feat: configure knex migration to production

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import { server } from './server/Server';
+import { Knex } from "./server/database/knex";
+import { server } from "./server/Server";
 
 const port = process.env.PORT || 3333;
 
-server.listen(port, () => console.log(`O servidor está rodando na porta ${port}`));
+const startServer = () => {
+  server.listen(port, () =>
+    console.log(`O servidor está rodando na porta ${port}`)
+  );
+};
+
+if (process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate
+    .latest()
+    .then(() => startServer())
+    .catch(() => console.log);
+} else {
+  startServer();
+}
