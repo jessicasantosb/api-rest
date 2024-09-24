14ddfebf37f836eb6c056ceda54a80e4dfda42bb
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-24 10:42:22 -0300

    feat: get all cities provider

--- src/server/controllers/cities/GetAll.ts
+++ src/server/controllers/cities/GetAll.ts
@@ -2,9 +2,11 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
+import { CitiesProvider } from "../../database/providers/cities";
 import { validation } from "../../shared/middleware";
 
 interface IQueryProps {
+  id?: number;
   page?: number;
   limit?: number;
   filter?: string;
@@ -13,6 +15,7 @@ interface IQueryProps {
 export const getAllBodyValidation = validation((getSchema) => ({
   query: getSchema<IQueryProps>(
     yup.object().shape({
+      id: yup.number().integer().optional().default(0),
       page: yup.number().optional().moreThan(0),
       limit: yup.number().optional().moreThan(0),
       filter: yup.string().optional(),
@@ -24,8 +27,28 @@ export const getAll = async (
   req: Request<{}, {}, {}, IQueryProps>,
   res: Response
 ) => {
-  res.setHeader("access-control-expose-headers", "x-total-count");
-  res.setHeader("x-total-count", 1);
+  const result = await CitiesProvider.getAll(
+    req.query.page || 1,
+    req.query.limit || 7,
+    req.query.filter || "",
+    Number(req.query.id)
+  );
+  const count = await CitiesProvider.count(req.query.filter);
 
-  return res.status(StatusCodes.OK).json([{ id: 1, nome: "Serro" }]);
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: { default: result.message },
+    });
+  }
+
+  if (count instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: { default: count.message },
+    });
+  }
+
+  res.setHeader("access-control-expose-headers", "x-total-count");
+  res.setHeader("x-total-count", count);
+
+  return res.status(StatusCodes.OK).json(result);
 };
--- src/server/database/providers/cities/index.ts
+++ src/server/database/providers/cities/index.ts
@@ -1,12 +1,14 @@
+import * as count from "./Count";
 import * as create from "./Create";
 import * as deleteById from "./DeleteById";
-// import * as getAll from "./GetAll";
+import * as getAll from "./GetAll";
 import * as getById from "./GetById";
 import * as updateById from "./UpdateById";
 
 export const CitiesProvider = {
   ...create,
-  // ...getAll,
+  ...count,
+  ...getAll,
   ...getById,
   ...updateById,
   ...deleteById,
