f49a7938067db482afe2e174792629e7e3e591b8
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-26 15:53:50 -0300

    feat: create user model

--- src/server/database/ETableNames.ts
+++ src/server/database/ETableNames.ts
@@ -1,4 +1,5 @@
 export enum ETableNames {
   city = "city",
   person = "person",
+  user = 'user'
 }
---
+++ src/server/database/migrations/0002_create_user.ts
@@ -0,0 +1,21 @@
+import { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+export async function up(knex: Knex) {
+  return knex.schema
+    .createTable(ETableNames.user, (table) => {
+      table.bigIncrements("id").primary().index();
+      table.string("nome").notNullable().checkLength(">", 3);
+      table.string("senha").notNullable().checkLength(">", 6);
+      table.string("email").index().unique().notNullable().checkLength(">", 6);
+
+      table.comment("Table to store users in the schema");
+    })
+    .then(() => console.log(`# Created table ${ETableNames.user}`));
+}
+
+export async function down(knex: Knex) {
+  return knex.schema
+    .dropTable(ETableNames.user)
+    .then(() => console.log(`# Dropped table ${ETableNames.user}`));
+}
