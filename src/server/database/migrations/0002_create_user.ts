13487864c156113b35f1c915f25f4f5e53b92d91
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-26 18:31:25 -0300

    feat: update user schema

--- src/server/database/migrations/0002_create_user.ts
+++ src/server/database/migrations/0002_create_user.ts
@@ -5,9 +5,9 @@ export async function up(knex: Knex) {
   return knex.schema
     .createTable(ETableNames.user, (table) => {
       table.bigIncrements("id").primary().index();
-      table.string("nome").notNullable().checkLength(">", 3);
-      table.string("senha").notNullable().checkLength(">", 6);
-      table.string("email").index().unique().notNullable().checkLength(">", 6);
+      table.string("nome").notNullable().checkLength(">=", 3);
+      table.string("senha").notNullable().checkLength(">=", 6);
+      table.string("email").index().unique().notNullable().checkLength(">=", 6);
 
       table.comment("Table to store users in the schema");
     })
