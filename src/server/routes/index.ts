33b7d876302e51ef9a9c996f45180a99b533fdf6
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-27 16:21:24 -0300

    feat: add ensure authentication

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { CitiesControllers, PeopleControllers, UsersControllers } from "../controllers";
+
+import {
+  CitiesControllers,
+  PeopleControllers,
+  UsersControllers,
+} from "../controllers";
+import { ensureAuthenticated } from "../shared/middleware";
 
 const router = Router();
 
@@ -9,52 +15,62 @@ router.get("/", (req, res) => {
 
 router.get(
   "/cities",
+  ensureAuthenticated,
   CitiesControllers.getAllBodyValidation,
   CitiesControllers.getAll
 );
 router.post(
   "/cities",
+  ensureAuthenticated,
   CitiesControllers.createBodyValidation,
   CitiesControllers.create
 );
 router.get(
   "/cities/:id",
+  ensureAuthenticated,
   CitiesControllers.getByIdBodyValidation,
   CitiesControllers.getById
 );
 router.put(
   "/cities/:id",
+  ensureAuthenticated,
   CitiesControllers.updateByIdBodyValidation,
   CitiesControllers.updateById
 );
 router.delete(
   "/cities/:id",
+  ensureAuthenticated,
   CitiesControllers.deleteByIdBodyValidation,
   CitiesControllers.deleteById
 );
 
 router.get(
   "/people",
+  ensureAuthenticated,
   PeopleControllers.getAllBodyValidation,
   PeopleControllers.getAll
 );
 router.post(
   "/people",
+  ensureAuthenticated,
   PeopleControllers.createBodyValidation,
   PeopleControllers.create
 );
 router.get(
   "/people/:id",
+  ensureAuthenticated,
   PeopleControllers.getByIdBodyValidation,
   PeopleControllers.getById
 );
 router.put(
   "/people/:id",
+  ensureAuthenticated,
   PeopleControllers.updateByIdBodyValidation,
   PeopleControllers.updateById
 );
 router.delete(
   "/people/:id",
+  ensureAuthenticated,
   PeopleControllers.deleteByIdBodyValidation,
   PeopleControllers.deleteById
 );
