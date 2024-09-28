8155720a3271d629729bf61cc46d5e6f4071f936
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-28 15:06:17 -0300

    fix: update query.id

--- src/server/controllers/cities/GetAll.ts
+++ src/server/controllers/cities/GetAll.ts
@@ -31,7 +31,7 @@ export const getAll = async (
     req.query.page || 1,
     req.query.limit || 7,
     req.query.filter || "",
-    Number(req.query.id)
+    Number(req.query.id || 0)
   );
   const count = await CitiesProvider.count(req.query.filter);
 
