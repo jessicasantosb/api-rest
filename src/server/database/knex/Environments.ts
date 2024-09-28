24ed1a0b9dd705223454a88100e461307c3074fc
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-28 14:51:36 -0300

    feat: add environments config for production

--- src/server/database/knex/Environments.ts
+++ src/server/database/knex/Environments.ts
@@ -30,4 +30,20 @@ export const development: Knex.Config = {
 
 export const test: Knex.Config = { ...development, connection: ":memory:" };
 
-export const production: Knex.Config = { ...development };
+export const production: Knex.Config = {
+  client: "pg",
+  migrations: {
+    directory: path.resolve(__dirname, "..", "migrations"),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "..", "seeds"),
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: process.env.DATABASE_SSL,
+  },
+};
--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,5 +1,12 @@
+import "dotenv/config";
 import knex from "knex";
+import pg from "pg";
+
 import { development, production, test } from "./Environments";
+
+if (process.env.NODE_ENV === "production") {
+  pg.types.setTypeParser(20, "text", parseInt);
+}
 
 const getEnvironments = () => {
   switch (process.env.NODE_ENV) {
