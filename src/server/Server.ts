ca551f068f310a683d864df6a1d7e8e91a95ade0
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-29 18:08:25 -0300

    feat: add cors configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,18 @@
-import 'dotenv/config';
-import express from 'express';
-import './shared/services/YupTranslation';
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
 
-import { router } from './routes';
+import "./shared/services/YupTranslation";
+
+import { router } from "./routes";
 
 const server = express();
+
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(";") || [],
+  })
+);
 
 server.use(express.json());
 server.use(router);
